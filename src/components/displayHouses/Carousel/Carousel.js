0bbe6a7525f3dd0b072c26931625da87db4ab824
Sylvestre <pasytchangwa@gmail.com>
2021-12-17 18:43:28 +0200

    Fix carousel

--- src/components/displayHouses/Carousel/Carousel.js
+++ src/components/displayHouses/Carousel/Carousel.js
@@ -24,11 +24,25 @@ const Carousel = ({ houses }) => {
     autoplay: true,
     autoplaySpeed: 2000,
     dots: true,
-    nextIcon: <NextArrow />,
-    prevIcon: <PrevArrow />,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
     responsive: [
       {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
         breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
