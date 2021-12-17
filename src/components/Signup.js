8f3d6e1105cfe8c2dc80b9a2f433c2e1d66709c2
Tolibjon <tolibjohn@gmail.com>
2021-12-17 17:04:16 +0500

    Create signup page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, Link } from 'react-router-dom';
+import { registerUser } from '../store/reducers/users';
 
-const Signup = () => <h1>Sign Up</h1>;
+const SignUp = () => {
+  const navigate = useNavigate();
 
-export default Signup;
+  const [username, setUsername] = useState();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.userReducer);
+
+  useEffect(() => {
+    if (user != null) { navigate('/'); }
+  }, [user]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(registerUser(username));
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h2>Signup</h2>
+        <span>Username</span>
+        <input type="text" onChange={(e) => setUsername(e.target.value)} />
+        <input type="submit" value="Signup" />
+      </form>
+      <Link to="/login"> Login </Link>
+    </div>
+  );
+};
+
+export default SignUp;
