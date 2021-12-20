9a2bee4a4e5e135ccf20ce1b0d2579cda31352f9
Tolibjon <tolibjohn@gmail.com>
2021-12-20 20:34:24 +0500

    Add availability field

--- src/components/AddHouses.js
+++ src/components/AddHouses.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { addHouse, fetchHouses } from '../store/action/houseActions';
+import { addHouse } from '../store/action/houseActions';
 
 const AddHouses = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const user = { id: 1 };
+  // const { user } = useSelector((state) => state.userReducer);
   const [house, setHouse] = useState({ user_id: user.id, availability: 12 });
 
   const handleNewHouse = (e) => {
@@ -16,7 +17,6 @@ const AddHouses = () => {
   const addNewHouse = (e) => {
     e.preventDefault();
     dispatch(addHouse(house));
-    dispatch(fetchHouses());
     navigate('/');
   };
 
@@ -27,6 +27,7 @@ const AddHouses = () => {
         <input type="text" name="title" onChange={handleNewHouse} placeholder="House title" required />
         <textarea onChange={handleNewHouse} name="house_description" placeholder="House description" required />
         <input type="text" name="location" onChange={handleNewHouse} placeholder="City" required />
+        <input type="number" name="availability" onChange={handleNewHouse} placeholder="Availability" min="1" required />
         <input type="text" name="image" onChange={handleNewHouse} placeholder="Image" required />
         <input type="number" name="price" onChange={handleNewHouse} placeholder="Price" required />
         <input type="number" min="0" name="discount" onChange={handleNewHouse} placeholder="Discount" required />
