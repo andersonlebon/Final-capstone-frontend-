4cfc8ac39f727c7f89e8da017857aaaf97dc47a8
Caleb Anderson <buyananderson@gmail.com>
2021-12-21 21:39:41 +0200

    Start styling My reervation page

--- src/components/MyReservations.jsx
+++ src/components/MyReservations.jsx
@@ -15,24 +15,37 @@ const Myreservations = () => {
   return (
     <div>
       <h2>My Reservations</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Duration</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          {reservationsReducer.map((reservation) => (
-            <tr key={reservation.id}>
-              <td>{reservation.title}</td>
-              <td>{reservation.duration}</td>
-              <td>{reservation.price}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <ul className="allReserved">
+        {reservationsReducer.map((reservation) => (
+          <li key={reservation.id}>
+            <div className="reserved-card">
+              <div className="reserved-img">
+                <img src={reservation.house.image} alt="reservation" />
+              </div>
+              <div className="reservation-info">
+                <h3 className="title">{reservation.house.title}</h3>
+                <p>{reservation.house.house_description}</p>
+                <ul className="reserved-info-list">
+                  <li>
+                    <span>Rent duration: </span>
+                    {reservation.rent_duration}
+                  </li>
+                  <li>
+                    <span>Start day: </span>
+                    {reservation.rent_start_date}
+                  </li>
+                  <li>
+                    <span>Total price: </span>
+                    {reservation.rent_duration
+                      * parseInt(reservation.house.price, 10)}
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <button type="button">Cancel Reservation </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- src/store/baseApi.js
+++ src/store/baseApi.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://house-booking-api.herokuapp.com/',
+  baseURL: 'http://localhost:3000/',
 });
