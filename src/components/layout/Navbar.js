83e97ef0b3bcff055ea8b61043c23775f9a79f83
Sylvestre <pasytchangwa@gmail.com>
2021-12-22 18:16:39 +0200

    Update user name

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-undef */
 import React from 'react';
+import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import logo from './logo.png';
@@ -10,57 +11,60 @@ import {
   Bars, NavLink, NavMenu, MobileIcon, UserIcon, UserCircle,
 } from './NavbarElelments';
 
-const Navbar = ({ logo, toggle }) => (
-  <nav className={style.navbar}>
-    <div className={style.nav_head}>
-      <h1 className={style.head}>
-        <Link to="/" className={style.logo_div}>
-          <img src={logo} alt="logo" width={100} height={100} />
-        </Link>
-        <UserIcon>
-          <UserCircle />
-          <span className={style.cur_user}>user</span>
-        </UserIcon>
-        <MobileIcon onClick={toggle}>
-          <Bars />
-        </MobileIcon>
-      </h1>
-      <div className={style.nav_body}>
-        <NavMenu className={style.navlist}>
-          <NavLink className={style.items} to="/" activestyle>
-            Houses
-          </NavLink>
+const Navbar = ({ logo, toggle }) => {
+  const { user } = useSelector((state) => state.userReducer);
+  return (
+    <nav className={style.navbar}>
+      <div className={style.nav_head}>
+        <h1 className={style.head}>
+          <Link to="/" className={style.logo_div}>
+            <img src={logo} alt="logo" width={100} height={100} />
+          </Link>
+          <UserIcon>
+            <UserCircle />
+            <span className={style.cur_user}>{user.name}</span>
+          </UserIcon>
+          <MobileIcon onClick={toggle}>
+            <Bars />
+          </MobileIcon>
+        </h1>
+        <div className={style.nav_body}>
+          <NavMenu className={style.navlist}>
+            <NavLink className={style.items} to="/" activestyle>
+              Houses
+            </NavLink>
 
-          <NavLink className={style.items} to="/myreservations" activestyle>
-            Reservations
-          </NavLink>
+            <NavLink className={style.items} to="/myreservations" activestyle>
+              Reservations
+            </NavLink>
 
-          <NavLink className={style.items} to="/addhouse" activestyle>
-            Add House
-          </NavLink>
+            <NavLink className={style.items} to="/addhouse" activestyle>
+              Add House
+            </NavLink>
 
-          <NavLink className={style.items} to="/removehouse" activestyle>
-            Remove House
-          </NavLink>
+            <NavLink className={style.items} to="/removehouse" activestyle>
+              Remove House
+            </NavLink>
 
-          <NavLink className={style.items} to="/reserve" activestyle>
-            Reserve
-          </NavLink>
-        </NavMenu>
+            <NavLink className={style.items} to="/reserve" activestyle>
+              Reserve
+            </NavLink>
+          </NavMenu>
+        </div>
       </div>
-    </div>
-    <footer className={style.footer}>
-      <SocialIcons />
-      <p>
-        &copy;
-        {' '}
-        <span>{new Date().getFullYear()}</span>
-        {' '}
-        all rights reserved
-      </p>
-    </footer>
-  </nav>
-);
+      <footer className={style.footer}>
+        <SocialIcons />
+        <p>
+          &copy;
+          {' '}
+          <span>{new Date().getFullYear()}</span>
+          {' '}
+          all rights reserved
+        </p>
+      </footer>
+    </nav>
+  );
+};
 
 Navbar.defaultProps = {
   logo,
