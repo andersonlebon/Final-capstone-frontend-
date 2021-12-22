bba0818e5f87a51e72e74871e545b8ff0d552b7b
Sylvestre <pasytchangwa@gmail.com>
2021-12-22 14:21:13 +0200

    removehouse mocks

---
+++ src/components/RemoveHouses/__mocks__/RemoveHouses.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RemoveHouses = () => (
+  <div className="container">
+    <h1 className="page_title">Remove house</h1>
+    <u />
+    <p>You do not have houses</p>
+  </div>
+);
+
+export default RemoveHouses;
